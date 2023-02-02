49830d1b236288b5bf3dae860aef784bdab5ffa0
ThomasGorisseGit <91067467+ThomasGorisseGit@users.noreply.github.com>
2023-02-02 10:05:55 +0100

    Pokae

--- script/scroll.js
+++ script/scroll.js
@@ -4,7 +4,6 @@
  */
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach(element => {
-        console.log(element);
         if(element.isIntersecting){
             element.target.classList.add('show');
         }else{
