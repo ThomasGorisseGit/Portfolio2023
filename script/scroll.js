7301afd2689d96d2445fe5214ec37f19938aa271
Thomas Gorisse <91067467+ThomasGorisseGit@users.noreply.github.com>
2024-01-27 12:53:53 +0100

    Update project

--- script/scroll.js
+++ script/scroll.js
@@ -11,9 +11,27 @@ const observer = new IntersectionObserver((entries)=>{
 
         }
     });
+
+});
+const bgAnimation = new IntersectionObserver((entries)=>{
+
+    entries.forEach(element => {
+        if(element.isIntersecting){
+            element.target.classList.add('bg-animation');
+        }else{
+            element.target.classList.remove('bg-animation');
+
+        }
+    });
+
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=>{
     observer.observe(el);
+});
+
+const background = document.querySelectorAll('.config');
+background.forEach((el)=>{
+    bgAnimation.observe(el);
 });
